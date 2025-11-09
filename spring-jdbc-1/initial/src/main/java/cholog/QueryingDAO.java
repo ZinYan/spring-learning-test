b314479e6a1635823026ce47ce98ac2a64e112e5
ZinYan <zinwaiyan2226@gmail.com>
2025-11-09 17:54:45 +0900

    Object with RowMapper 학습 테스트

--- spring-jdbc-1/initial/src/main/java/cholog/QueryingDAO.java
+++ spring-jdbc-1/initial/src/main/java/cholog/QueryingDAO.java
@@ -50,7 +50,16 @@ public class QueryingDAO {
     public Customer findCustomerById(Long id) {
         String sql = "select id, first_name, last_name from customers where id = ?";
         //TODO : 주어진 Id에 해당하는 customer를 객체로 반환
-        return null;
+        return jdbcTemplate.queryForObject(
+                sql,
+            (resultSet, rowNum)-> {
+                 Customer customer = new Customer(
+                        resultSet.getLong("id"),
+                        resultSet.getString("first_name"),
+                        resultSet.getString("last_name")
+                );
+                return customer;
+            },id);
     }
 
     /**
